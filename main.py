e8a04dc0f940683acb16b5f4f2a80d214026d840
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2023-08-13 01:36:06 +0530

    removed unused code 1.

--- main.py
+++ main.py
@@ -46,37 +46,6 @@ class App(ctk.CTk):
         self.pccontrol_var = ctk.BooleanVar(value=False)#False)
         self.mouseSmoothness_var = ctk.IntVar(value=DEFAULT_MOUSE_SMOOTHNESS)
         
-        # tracing
-        self.videoOnOff_var.trace('w', 
-                                  self.turn_on_off_video)
-        self.handlandmarkOnOff_var.trace('w', 
-                                         self.show_handlandmarks)
-        self.pccontrol_var.trace('w', 
-                                 self.pccontrol_func) 
-        self.mouseSmoothness_var.trace('w', 
-                                       self.change_mouse_smoothness)
-        
-    
-    def change_mouse_smoothness(self, *args):
-        pass
-
-    def turn_on_off_video(self, *args):
-        # print(self.videoOnOff_var.get())
-        # SHOW_VIDEO = self.videoOnOff_var.get()
-        # print('xxxx: ', self.videoOnOff_var.get())
-        pass
-
-    def show_handlandmarks(self, *args):
-        # print(self.handlandmarkOnOff_var.get())
-        # SHOW_HANDLANDMARKS = self.handlandmarkOnOff_var.get()
-        # print('yyyy: ', self.handlandmarkOnOff_var.get())
-        pass
-
-    def pccontrol_func(self, *args):
-        # FUNCTIONING_ON = self.pccontrol_var.get()
-        # print('zzzz: ', self.pccontrol_var.get())
-        pass
-
 # main
 if __name__ == '__main__':
     App()
